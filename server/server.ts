2121e8f4336bb41d4369e00282ea1a2f7f118b44
DivyamOswal <divyam9850400550@gmail.com>
2026-07-17 13:56:53 +0530

    feat: implement user authentication with registration, login, logout, and verification endpoints; add session management and protect middleware

--- server/server.ts
+++ server/server.ts
@@ -5,10 +5,11 @@ import 'dotenv/config'
 import connectDB from "./config/db.js";
 import session from "express-session";
 import MongoStore from "connect-mongo";
+import authRouter from "./routes/authRoutes.js";
 
 declare module 'express-session'{
     interface SessionData {
-        isLogged: boolean;
+        isLoggedIn: boolean;
         userId: string
     }
 }
@@ -37,6 +38,7 @@ const port = process.env.PORT || 3000;
 app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
 });
+app.use('/api/auth', authRouter)
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
